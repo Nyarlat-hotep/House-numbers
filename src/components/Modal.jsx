1946beaffb6d0c56b15c3432f74c865b76b91e21
Taylor Cornelius <tcorneliusart@gmail.com>
2026-03-25 21:19:40 -0400

    feat: add shared Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,12 @@
+import './Modal.css'
+
+export default function Modal({ title, onClose, children }) {
+  return (
+    <div className="modal-overlay" onClick={(e) => e.target === e.currentTarget && onClose()}>
+      <div className="modal">
+        <div className="modal-title">{title}</div>
+        {children}
+      </div>
+    </div>
+  )
+}
